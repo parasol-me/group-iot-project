55e133fcfbe8cc9b1dacda1d383c377b51393acb
101106284Kayla <37137465+101106284Kayla@users.noreply.github.com>
2022-05-16 18:36:57 +1000

    prints only when sound detected

--- EdgeDevice_Kayla/SoundSensor/SoundSensor.ino
+++ EdgeDevice_Kayla/SoundSensor/SoundSensor.ino
@@ -1,21 +1,28 @@
 
 //LED pin
 int redLEDPin = 9;
+int yellowPin = 5;
 
 //Sound Sensor pin
 int micPin = A0;
-//int digitalPin = 11;
+int digitalPin = 2;
 int sensorValue = 0;
 int DigitalState = 0;
+int Threshold = 522;
 
 void setup() {
   //Baud rate
   Serial.begin(9600);
 
+  //Sound sensor
+  pinMode(digitalPin, INPUT);
+
   //LED
   pinMode(redLEDPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
   //Set intial LED state
   digitalWrite(redLEDPin, LOW);
+  digitalWrite(yellowPin, LOW);
 
 }
 
@@ -24,17 +31,28 @@ void loop() {
 
   //---- Sound Sensor Siren -----
   sensorValue = analogRead(micPin);
-  //DigitalState = digitalRead(digitalPin);
-  Serial.println (sensorValue, DEC); //analogueRead from microphone sound sensor
+  DigitalState = digitalRead(digitalPin);
+  //Serial.println (sensorValue, DEC); //analogueRead from microphone sound sensor
 
-  if(sensorValue > 522) //if sound analogue detects highvoltage (eg. loud sounds) then turn red light on
+  //Serial.println(DigitalState);
+  //digitalWrite(redLEDPin, DigitalState);
+
+  if(DigitalState == 1)
+  {
+    //Serial.println(DigitalState);
+
+    if(sensorValue > Threshold) //if sound analogue detects highvoltage (eg. loud sounds) then turn red light on
     {
       digitalWrite(redLEDPin, HIGH);
+      Serial.println(sensorValue);
     } else
     {
       digitalWrite(redLEDPin, LOW);
     }
-  delay(1000);
+    //delay(250);
+  }
+
+  
 
 
 }
