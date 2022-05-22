a2a4816a0af33047f31f3fd43ab55373249be301
Jason <48763714+Jase-99@users.noreply.github.com>
2022-05-22 23:22:38 +1000

    Final Push

--- EdgeDevice_Jason/iotNodeScript/jasonNodeScript.ino
+++ EdgeDevice_Jason/iotNodeScript/jasonNodeScript.ino
@@ -3,13 +3,19 @@ dht DHT;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(4,OUTPUT);
 }
 
 void loop() {
   int chk = DHT.read11(2); //Reads from pin 2
-  Serial.print("Temperature = ");
   Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
   delay(1000);
+  
+  int message = Serial.read();
+  if(message == '1'){
+      digitalWrite(4,HIGH);
+    }else if(message == '2'){
+      digitalWrite(4,LOW);
+    } 
 }
